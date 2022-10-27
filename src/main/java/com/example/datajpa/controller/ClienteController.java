4219adbabdfde95eb5eda5e4576d2f0862228b12
Danny Tenemea <danny.tenemea.est@tecazuay.edu.ec>
2022-10-27 18:29:26 -0500

    new commit

---
+++ src/main/java/com/example/datajpa/controller/ClienteController.java
@@ -0,0 +1,25 @@
+package com.example.datajpa.controller;
+
+import com.example.datajpa.dao.IClienteDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ClienteController {
+
+    @Autowired
+    private IClienteDao clienteDao;
+
+    @RequestMapping(value = "listar", method = RequestMethod.GET)
+    public String listar(Model model){
+        model.addAttribute("titulo", "listado de clientes");
+        model.addAttribute("clientes", clienteDao.findAll());
+
+        return "listar";
+    }
+
+
+}
