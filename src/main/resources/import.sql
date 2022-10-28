87fd676f472bd68496b1d6197333c62df0025f7a
Danny Tenemea <danny.tenemea.est@tecazuay.edu.ec>
2022-10-27 19:50:53 -0500

    commit bueno

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
--- INSERT INTO cliente(id,nombre,apellido,email,create_at) VALUES (1,'Danny','Tenemea','danny@gmail.com','1999-17-11');
+INSERT INTO cliente(id,nombre,apellido,email,create_at) VALUES (1,'Danny','Tenemea','danny@gmail.com','1999-11-17');
 INSERT INTO cliente(id,nombre,apellido,email,create_at) VALUES (2,'Marlon','Velez','marlon@gmail.com','2000-09-12');
--- INSERT INTO cliente(id,nombre,apellido,email,create_at) VALUES (3,'Paola','Criollo','paola@gmail.com','2000-31-01');
+INSERT INTO cliente(id,nombre,apellido,email,create_at) VALUES (3,'Paola','Criollo','paola@gmail.com','2000-01-31');
 INSERT INTO cliente(id,nombre,apellido,email,create_at) VALUES (4,'William','Tocto','william@gmail.com','1998-07-11');
--- INSERT INTO cliente(id,nombre,apellido,email,create_at) VALUES (5,'Jose','Murillo','jose@gmail.com','2001-17-01');
+INSERT INTO cliente(id,nombre,apellido,email,create_at) VALUES (5,'Jose','Murillo','jose@gmail.com','2001-01-17');
